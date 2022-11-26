8f8a14f4e029fb18ffb4c0cfcd2d1365290a22ee
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 19:49:10 -0300

    aula 03 commit

---
+++ Javascript/javascript-basico/03-variaveis/index.js
@@ -0,0 +1,9 @@
+var nomePessoa = "Luciano";
+
+const emailPessoa = "lucianoscabora@gmail.com"
+
+let telefonePessoa = "5519997848703"
+
+nomePessoa = "Luciano Scabora"
+
+// const são variáveis que não podem ter os valores alterados, ao contrario de let e var
