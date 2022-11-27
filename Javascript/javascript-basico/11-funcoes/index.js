828d468caf99e148a7403b7ea929d2b855db5d66
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 21:24:21 -0300

    aula 11 commit

---
+++ Javascript/javascript-basico/11-funcoes/index.js
@@ -0,0 +1,35 @@
+/* Anatomia
+
+function nomeDaFuncao() {
+    <bloco de execucao>
+}
+
+nomeDaFuncao();
+*/
+
+/* Anatomia com parametro
+
+function nomeDaFuncao(parametro1, parametro2) {
+    <bloco de execucao>
+}
+
+nomeDaFuncao(parametro1, parametro2);
+*/
+
+
+function incentivarQuester(nomeQuester = 'Quester') {
+    alert('Você chegou ao JS, ' + nomeQuester + ', continue assim');
+}
+
+incentivarQuester('Luciano');
+
+
+/* function somar(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
+
+let resultadoDaSoma = somar(1, 3);
+console.log(resultadoDaSoma); //4
+
+resultadoDaSoma = somar(resultadoDaSoma, 10);
+console.log(resultadoDaSoma); // 14 */
