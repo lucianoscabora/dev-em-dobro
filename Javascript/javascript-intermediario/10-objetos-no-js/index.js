0fedfd5fe5ed9b0a33f4a33a9b14ac3b34e803e7
lucianoscabora <lucianoscabora@gmail.com>
2022-11-27 00:23:12 -0300

    aula 10 - js intermediario

--- Javascript/javascript-intermediario/10-objetos-no-js/index.js
+++ Javascript/javascript-intermediario/10-objetos-no-js/index.js
@@ -6,6 +6,11 @@ let tenis = {
     estoque: true
 }
 
+//adiciona uma nova chave e valor
 tenis.valor = 200;
 
+//imprime o objeto todo
 console.log(tenis);
+
+//imprime a chave escolhida
+console.log(tenis.marca);
