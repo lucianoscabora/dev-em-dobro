61962559750c34c926935e31da71ee35aec84562
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 23:01:03 -0300

    aula 04 - js intermediario

---
+++ Javascript/javascript-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,29 @@
+/* function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+let resultadoDaSoma = somar(12, 5)
+console.log(resultadoDaSoma) */
+
+// anonima
+
+/* let resultadoDaSoma = function(numero1, numero2) {
+    return numero1 + numero2;
+    
+}
+
+console.log(resultadoDaSoma(12, 5)) */
+
+// arrow function
+
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+console.log(resultadoDaSoma(12, 5))
+
+// como deixar ainda menor
+
+/* const incentivarQuester = nomeQuester => {
+    console.log('Parabéns ' + nomeQuester + ', Você chegou ao módulo de JS intermediário')
+}
+
+incentivarQuester('Luciano'); */
