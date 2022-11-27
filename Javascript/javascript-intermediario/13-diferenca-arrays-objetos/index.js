2daa4b5812548a0935f4c51983c61a2b25e04f35
lucianoscabora <lucianoscabora@gmail.com>
2022-11-27 01:04:28 -0300

    aula 13 - js intermediario

---
+++ Javascript/javascript-intermediario/13-diferenca-arrays-objetos/index.js
@@ -0,0 +1,16 @@
+let frutas = ['banana', 'uva', 'maca', 'melao'];
+
+let videogame = {
+    jogo: 'Playstation',
+    valor: 3000
+}
+
+let videogame2 = {
+    jogo: 'xbox',
+    valor: 2000
+}
+
+let videogames = [videogame, videogame2]
+
+/* console.log(videogames); */
+console.log(videogames[0].jogo)
