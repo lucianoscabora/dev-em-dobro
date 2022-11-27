5a51d6ffae4fff6bfb287451d6f45c2e98eeff60
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 21:00:25 -0300

    commit aula 09

---
+++ Javascript/javascript-basico/09-condicionais/index.js
@@ -0,0 +1,16 @@
+let nomeFilme = "Vingadores";
+
+if(nomeFilme === "Vingadores") {
+    console.log('É o filme dos Vingadores')  
+    
+}else if(nomeFilme === "Batman Vs Superman"){
+    console.log('É o filme do Batman');
+}else {
+    console.log('É um outro filme qualquer');
+}
+
+
+// Operadores ternários
+// Anatomia
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos vingadores') : console.log('É outro filme');
