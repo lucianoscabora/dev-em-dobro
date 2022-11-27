690e598bfa7249db69cdd6bcfb602077489efcb4
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 22:39:55 -0300

    aula 01 - js intermediario

---
+++ Javascript/javascript-intermediario/01-escopo-js/index.js
@@ -0,0 +1,36 @@
+/* if(true) {
+    const mensagem = "Olá";
+}
+
+console.log(mensagem); */
+
+/* {
+    var mensagem = "Olá";
+    console.log(mensagem);
+}
+
+    console.log(mensagem); */
+
+    /* function falar() {
+        var mensagem = 'olá mundo';
+        console.log(mensagem);
+    }
+
+    falar(); */
+
+    function falar() {
+        var mensagem = 'olá mundo';
+        console.log(mensagem);
+        function falarOutraCoisa() {
+
+        }
+
+        falarOutraCoisa()
+        console.log(falarOutraCoisa);
+    }
+
+    /* falar();
+    console.log(mensagem);
+    console.log(falarOutraCoisa); */
+    console.log(url);
+
