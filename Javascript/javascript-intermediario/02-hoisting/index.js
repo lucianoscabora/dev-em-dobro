6b9c13bea5af25d820f210a8b5790f6cb7195e81
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 22:45:37 -0300

    aula 02 - js intermediario

---
+++ Javascript/javascript-intermediario/02-hoisting/index.js
@@ -0,0 +1,14 @@
+/* var pais; // declaração
+pais = "Brasil" // atribuição
+
+var pais = "Brasil";
+
+ */
+
+console.log(pais);
+var pais = "Brasil";
+// da undefined porque o hoisting move a declaração da variável para cima, mas não a declaracão da mesma
+
+console.log(pais);
+let pais = "Brasil";
+// variáveis de let é içada mas não é atribuída com valor algum, por isso o erro é diferente
