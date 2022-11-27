0c065e72d2127d0835de4ef7d8d9b13a6cf4367b
lucianoscabora <lucianoscabora@gmail.com>
2022-11-26 23:47:49 -0300

    aula 07 - js intermediario

---
+++ Javascript/javascript-intermediario/07-arrays-multidimensionais/index.js
@@ -0,0 +1,7 @@
+let descricaoFilmes = [
+    ['Mulher Maravilha', 4.5, true, ['Gal Gadotti', 25]],
+    ['Batman', 5, false]
+                      ]
+
+
+    console.table(descricaoFilmes[0][3][0]);
