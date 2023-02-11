fb6db563717bc9f0cd03a6fa34b6153c244fa7e9
lucianoscabora <lucianoscabora@gmail.com>
2023-02-11 17:39:11 -0300

    estudo: aula 01 - react basico concluída

---
+++ react-basico/aula-01/src/App.js
@@ -0,0 +1,15 @@
+import Button from './components/button/button';
+
+function App() {
+  return (
+    <>
+  <Button />
+  <Button />
+  <Button />
+  <Button />
+  </>
+  );
+  
+}
+
+export default App;
