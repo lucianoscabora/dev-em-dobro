86ecf892d0dcea9b9bce380cdc4ec2cea06f7d4f
lucianoscabora <lucianoscabora@gmail.com>
2023-02-11 19:03:49 -0300

    aula 06 - react basico concluida

--- react-basico/aula-01/src/App.js
+++ react-basico/aula-01/src/App.js
@@ -1,26 +1,9 @@
-import Button from "./components/button/button";
-/* import Card from "./components/card/card"; */
+import Cards from "./components/cards/cards";
 
 function App() {
   return (
     <>
-      <Button></Button>
-      <Button label='Baixar CV'></Button>
-
-  {/*     <Card>
-        <h3>Titulo card 1</h3>
-        <p>Texto do card</p>
-      </Card>
-
-      <Card>
-        <h3>Titulo card 2</h3>
-        <p>Texto do card</p>
-      </Card>
-
-      <Card>
-        <h3>Titulo card 3</h3>
-        <p>Texto do card</p>
-      </Card> */}
+      <Cards />
     </>
   );
 }
--- react-basico/aula-01/src/components/button/button.js
+++ react-basico/aula-01/src/components/button/button.js
@@ -1,10 +1,6 @@
 import React from "react";
 import '../button/button.css'
 
-/* const Button = (props) => {
-return <button className="btn">{props.label}</button>
-} */
-
 class Button extends React.Component {
     render() {
         return <button className="btn">{this.props.label}</button>
